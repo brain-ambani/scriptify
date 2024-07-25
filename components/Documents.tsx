8f4d5a25a28adb595cf269cc91e588170dafc8fd
brain-ambani <brainambani1@gmail.com>
2024-07-25 06:14:27 +0300

    updated the dashboard

---
+++ components/Documents.tsx
@@ -0,0 +1,14 @@
+import PlaceholderDocument from "./PlaceholderDocument";
+
+const Documents = () => {
+  return (
+    <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto">
+      Documents
+      {/* Map through the documents */}
+      {/* Placeholder Documents */}
+      <PlaceholderDocument />
+    </div>
+  );
+};
+
+export default Documents;
