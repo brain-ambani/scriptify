239717d078d78bb665fec602963888ff86addc67
brain-ambani <brainambani1@gmail.com>
2024-07-25 06:17:44 +0300

    updated the documents file

--- components/Documents.tsx
+++ components/Documents.tsx
@@ -3,7 +3,6 @@ import PlaceholderDocument from "./PlaceholderDocument";
 const Documents = () => {
   return (
     <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto">
-      Documents
       {/* Map through the documents */}
       {/* Placeholder Documents */}
       <PlaceholderDocument />
